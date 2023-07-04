e80be67307c6c9664925c355f7fc05ab56e1ff14
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-07-04 21:28:39 +0600

    Add movielist

--- src/pages/Movielist.js
+++ src/pages/Movielist.js
@@ -1,5 +1,15 @@
+import { Card } from "../components/Card";
+
 export const Movielist = () => {
   return (
-    <main>Movielist</main>
-  )
-}
+    <main>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-start">
+          <Card/>
+          <Card/>
+          <Card/>
+        </div>
+      </section>
+    </main>
+  );
+};
