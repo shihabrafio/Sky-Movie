f9956b7a1c7661e90541468dc085423e6b2dfa96
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-07-05 00:29:25 +0600

    Add cards for demo

--- src/pages/Movielist.js
+++ src/pages/Movielist.js
@@ -4,10 +4,15 @@ export const Movielist = () => {
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
-        <div className="flex justify-start flex-start">
-          <Card/>
-          <Card/>
-          <Card/>
+        <div className="flex justify-start">
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </div>
         </div>
       </section>
     </main>
