ee44f65a5d30e6e90af0d9a5051beff2dc422282
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-07-05 23:58:08 +0600

    Add props to the element

--- src/Routes/Allroutes.js
+++ src/Routes/Allroutes.js
@@ -9,13 +9,13 @@ export const Allroutes = () => {
   return (
     <div className="dark:bg-slate-800">
     <Routes>
-        <Route path="/" element={<Movielist/>} />
+        <Route path="/" element={<Movielist apiPath="movie/now_playing"/>}  />
         <Route path="movie/:id" element={<MovieDetail/>} />
-        <Route path="movies/popular" element={<Movielist/>} />
-        <Route path="movies/upcoming" element={<Movielist/>} />
-        <Route path="movies/top" element={<Movielist/>} />
+        <Route path="movies/popular" element={<Movielist apiPath="movie/popular"/>} />
+        <Route path="movies/upcoming" element={<Movielist apiPath="movie/upcoming"/>}  />
+        <Route path="movies/top" element={<Movielist apiPath="movie/top_rated"/>}  />
         <Route path="search" element={<Search/>} />
-        <Route path="*" element={<PageNotFound/>} />
+        <Route path="*" element={<PageNotFound apiPath="search/movie"/>} />
     </Routes>
     </div>
   )
