c3149cd73e43af87062ca9b27decdb871e18bebe
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-07-06 16:52:49 +0600

    Remove flex

--- src/pages/Movielist.js
+++ src/pages/Movielist.js
@@ -7,13 +7,11 @@ export const Movielist = ({apiPath}) => {
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
-        <div className="flex justify-start">
           <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
             {movies.map((movie)=>(
             <Card key={movie.id} movie={movie} />
             ))}
           </div>
-        </div>
       </section>
     </main>
   );
