7b143eb8db928e201f0954c25a9536f77301314b
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-07-08 01:11:13 +0600

    Add customized background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     darkMode: 'class',
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          darkbg: '#1E293B',
+        }
+      },
     },
     plugins: [],
   }
