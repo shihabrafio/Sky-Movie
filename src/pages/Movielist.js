c600b29a7ef25953dddf151b99c22cfe847e85ba
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-07-08 21:06:43 +0600

    Update Movielist

--- src/pages/Movielist.js
+++ src/pages/Movielist.js
@@ -2,19 +2,23 @@ import { Card } from "../components/Card";
 import { useFetch } from "../hooks/useFetch";
 import { useTitle } from "../hooks/useTitle";
 
-export const Movielist = ({apiPath,title}) => {
-  const {data: movies} = useFetch(apiPath)
+export const Movielist = ({ apiPath, title }) => {
+  const { data: movies } = useFetch(apiPath);
 
-  useTitle(title)
+  useTitle(title);
+
+  if (!movies || movies.length === 0) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-            {movies.map((movie)=>(
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+          {movies.map((movie) => (
             <Card key={movie.id} movie={movie} />
-            ))}
-          </div>
+          ))}
+        </div>
       </section>
     </main>
   );
