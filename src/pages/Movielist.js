522e8b21850ef251ec477f930669d74485029335
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-07-08 21:30:06 +0600

    Update Movielist

--- src/pages/Movielist.js
+++ src/pages/Movielist.js
@@ -14,7 +14,7 @@ export const Movielist = ({ apiPath, title }) => {
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           {movies.map((movie) => (
             <Card key={movie.id} movie={movie} />
           ))}
