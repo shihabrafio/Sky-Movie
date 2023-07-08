82351af3b67a2a8161fcb2d6d3c0d78963208f55
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-07-08 21:00:39 +0600

    Update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,7 +36,7 @@ export const Header = () => {
           <Link to="/" className="flex items-center">
             <img src={logo} className="h-8 mr-3" alt="MovieAPI Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              MovieAPI
+              Sky Movie
             </span>
           </Link>
 
